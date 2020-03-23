1ae6dff71a787849a54fa5c4b2b3e893a53465e2
Evan Byrd <elbyrd09@gmail.com>
2020-03-23 16:20:36 -0400

    created all listed in branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  get 'work/index'
+  get 'work/show'
+  get 'work/new'
+  get 'work/create'
+  get 'work/edit'
+  get 'work/update'
+  get 'work/destroy'
+  get 'blog/index'
+  get 'blog/show'
+  get 'blog/new'
+  get 'blog/create'
+  get 'blog/edit'
+  get 'blog/update'
+  get 'blog/destroy'
+  root to: 'pages#home'
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
   get 'work', to: 'pages#work', as: :work
